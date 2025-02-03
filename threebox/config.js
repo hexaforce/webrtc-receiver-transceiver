f71dba9190f42af909635f208e6a0ab8a8f0285a
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-03 23:43:27 +0900

    xx

--- threebox/config.js
+++ threebox/config.js
@@ -1,4 +1,4 @@
 var config = {
-    accessToken: 'pk.eyJ1IjoicmVsaWNzOSIsImEiOiJjbHMzNHlwbDIwNDczMmtvM2xhNWR0ZzVtIn0.whCzeh6XW7ju4Ja6DR0imw',
-    subscriptionKey: 'azure maps subscription key goes here'
-};
+  accessToken: 'pk.eyJ1IjoicmVsaWNzOSIsImEiOiJjbHMzNHlwbDIwNDczMmtvM2xhNWR0ZzVtIn0.whCzeh6XW7ju4Ja6DR0imw',
+  subscriptionKey: 'azure maps subscription key goes here',
+}
