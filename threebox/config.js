d02bbc22e61dfb1cd6b48d2f3ccf10e22f5f8647
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-04 09:47:34 +0900

    xx

--- threebox/config.js
+++ threebox/config.js
@@ -1,4 +1,36 @@
 var config = {
-  accessToken: 'pk.eyJ1IjoicmVsaWNzOSIsImEiOiJjbHMzNHlwbDIwNDczMmtvM2xhNWR0ZzVtIn0.whCzeh6XW7ju4Ja6DR0imw',
-  subscriptionKey: 'azure maps subscription key goes here',
-}
+  accessToken:
+    "pk.eyJ1IjoicmVsaWNzOSIsImEiOiJjbHMzNHlwbDIwNDczMmtvM2xhNWR0ZzVtIn0.whCzeh6XW7ju4Ja6DR0imw",
+  subscriptionKey: "azure maps subscription key goes here",
+  getPosition: async (timeout) => {
+    if (!window.navigator || !window.navigator.geolocation)
+      throw new Error("Geolocation is not supported by this browser.");
+    return new Promise((resolve, reject) => {
+      window.navigator.geolocation.getCurrentPosition(
+        ({ coords, timestamp }) => {
+          const {
+            accuracy,
+            altitude,
+            altitudeAccuracy,
+            heading,
+            latitude,
+            longitude,
+            speed,
+          } = coords;
+          resolve({
+            accuracy,
+            altitude,
+            altitudeAccuracy,
+            heading,
+            latitude,
+            longitude,
+            speed,
+            timestamp,
+          });
+        },
+        (err) => reject(err),
+        // { enableHighAccuracy: true, timeout: timeout, maximumAge: 0 }
+      );
+    });
+  },
+};
