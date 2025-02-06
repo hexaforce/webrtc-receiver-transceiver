b812481bd45fca9f0ae5db16fd91c94354bda646
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-06 20:54:08 +0900

    update

--- fpv/common.js
+++ fpv/common.js
@@ -2,21 +2,21 @@ const $ = (id) => document.getElementById(id)
 
 const dataChannel = {}
 
-const iceCandidateHandler = (conn, ws) => {
-  conn.onicecandidate = ({ candidate }) => {
+const iceCandidateHandler = (pc, ws) => {
+  pc.onicecandidate = ({ candidate }) => {
     console.log('ice answer:', candidate)
     candidate && ws.send(JSON.stringify(candidate))
   }
 }
 
-const dataChannelHandler = (conn, PROTOCOL) => {
-  conn.ondatachannel = ({ channel }) => {
+const dataChannelHandler = (pc, PROTOCOL) => {
+  pc.ondatachannel = ({ channel }) => {
     channel.onopen = ({ target }) => (dataChannel[target.label] = target)
     channel.onmessage = ({ data }) => {
       console.log(data)
     }
   }
-  const channel = conn.createDataChannel(PROTOCOL)
+  const channel = pc.createDataChannel(PROTOCOL)
   channel.onopen = ({ target }) => (dataChannel[target.label] = target)
   channel.onmessage = ({ data }) => {
     console.log(data)
@@ -27,39 +27,40 @@ const dataChannelHandler = (conn, PROTOCOL) => {
 //   dataChannel[PROTOCOL].send(data)
 // }
 
-const setReceiverAnswerCodec = async (conn) => {
+const setReceiverAnswerCodec = async (pc) => {
   const supportedCodecs = RTCRtpReceiver.getCapabilities('video').codecs
   const preferredOrder = ['video/AV1', 'video/VP9', 'video/H265', 'video/H264', 'video/VP8']
-  const [audioTransceiver, videoTransceiver] = conn.getTransceivers()
+  const [audioTransceiver, videoTransceiver] = pc.getTransceivers()
   if (videoTransceiver) {
-    videoTransceiver.setCodecPreferences(supportedCodecs.sort((a, b) => {
-      const indexA = preferredOrder.indexOf(a.mimeType)
-      const indexB = preferredOrder.indexOf(b.mimeType)
-      const orderA = indexA >= 0 ? indexA : Number.MAX_VALUE
-      const orderB = indexB >= 0 ? indexB : Number.MAX_VALUE
-      return orderA - orderB
-    }))
+    videoTransceiver.setCodecPreferences(
+      supportedCodecs.sort((a, b) => {
+        const indexA = preferredOrder.indexOf(a.mimeType)
+        const indexB = preferredOrder.indexOf(b.mimeType)
+        const orderA = indexA >= 0 ? indexA : Number.MAX_VALUE
+        const orderB = indexB >= 0 ? indexB : Number.MAX_VALUE
+        return orderA - orderB
+      }),
+    )
   }
 }
 
-const setMediaTransceiver = async (stream, conn, ws) => {
+const setMediaTransceiver = async (stream, pc, ws) => {
   stream.getTracks().forEach((track) => {
-    conn.addTrack(track, stream)
+    pc.addTrack(track, stream)
     console.log('addTrack:', stream)
   })
   ws.onclose = () => stream.getTracks().forEach((track) => track.stop())
-  conn.getTransceivers().forEach((transceiver) => {
+  pc.getTransceivers().forEach((transceiver) => {
     transceiver.direction = 'sendonly'
   })
 }
 
-const setMediaReceiver = async (video, conn, ws) => {
-  conn.ontrack = ({ streams }) => {
+const setMediaReceiver = async (video, pc, ws) => {
+  pc.ontrack = ({ streams }) => {
     video.srcObject = streams[0]
-    console.log('ontrack:', video.srcObject)
   }
   ws.onclose = () => video.srcObject.getTracks().forEach((track) => track.stop())
-  conn.getTransceivers().forEach((transceiver) => {
+  pc.getTransceivers().forEach((transceiver) => {
     transceiver.direction = 'recvonly'
   })
 }
