1786e0fb42d0d20978f3179a1f8b2f5a3925d11e
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-08 20:29:44 +0900

    xxx

--- d3/script.js
+++ d3/script.js
@@ -20,7 +20,7 @@ function createSvg(id) {
   return svg
 }
 
-function chart(id, xDomain, yDomain, interpolation, tick) {
+function lineChart(id, xDomain, yDomain, interpolation, tick) {
   var data = d3.range(n).map(random)
 
   var x = d3.scale.linear().domain(xDomain).range([0, width])
@@ -29,8 +29,38 @@ function chart(id, xDomain, yDomain, interpolation, tick) {
   var svg = createSvg(id)
   svg.append('g').attr('class', 'y axis').call(d3.svg.axis().scale(y).ticks(5).orient('left'))
 
-  var line = d3.svg.line().interpolate(interpolation).x((d, i) => x(i)).y((d) => y(d))
+  var line = d3.svg
+    .line()
+    .interpolate(interpolation)
+    .x((d, i) => x(i))
+    .y((d) => y(d))
   var path = svg.append('g').attr('clip-path', 'url(#clip)').append('path').datum(data).attr('class', 'line').attr('d', line)
 
   tick(path, line, data, x)
+}
+
+function barChart(id, xDomain, yDomain, tick) {
+  var data = d3.range(n).map(random)
+
+  var x = d3.scale.linear().domain(xDomain).range([0, width])
+  var y = d3.scale.linear().domain(yDomain).range([height, 0])
+
+  var svg = createSvg(id)
+  svg.append('g').attr('class', 'y axis').call(d3.svg.axis().scale(y).ticks(5).orient('left'))
+
+  var bars = svg
+    .append('g')
+    .attr('class', 'bars')
+    .selectAll('.bar')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (d, i) => x(i))
+    .attr('y', (d) => y(d))
+    .attr('width', width / n - 1)
+    .attr('height', (d) => height - y(d))
+    .attr('fill', 'steelblue')
+
+  tick(bars, data, x, y)
 }
