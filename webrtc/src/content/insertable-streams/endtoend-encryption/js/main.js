0e718ed70cf44c7adbded48d6c4ec7629d93d527
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-22 18:44:40 +0900

    xxx

--- webrtc/src/content/insertable-streams/endtoend-encryption/js/main.js
+++ webrtc/src/content/insertable-streams/endtoend-encryption/js/main.js
@@ -11,17 +11,6 @@ const startButton = document.getElementById("startButton");
 const callButton = document.getElementById("callButton");
 const hangupButton = document.getElementById("hangupButton");
 
-let startToMiddle;
-let startToEnd;
-
-let localStream;
-let remoteStream;
-
-if (!!!window.RTCRtpScriptTransform) {
-  const stream = new ReadableStream();
-  window.postMessage(stream, "*", [stream]);
-}
-
 startButton.onclick = function start() {
   console.log("Requesting local stream");
   startButton.disabled = true;
@@ -39,6 +28,34 @@ startButton.onclick = function start() {
       console.log("getUserMedia() error: ", e);
     });
 };
+
+function VideoPipe(stream, forceSend, forceReceive, handler) {
+  this.pc1 = new RTCPeerConnection({ encodedInsertableStreams: forceSend });
+  this.pc2 = new RTCPeerConnection({ encodedInsertableStreams: forceReceive });
+  this.pc2.ontrack = handler;
+  stream.getTracks().forEach((track) => this.pc1.addTrack(track, stream));
+}
+
+VideoPipe.prototype.negotiate = async function () {
+  this.pc1.onicecandidate = ({ candidate }) =>
+    this.pc2.addIceCandidate(candidate);
+  this.pc2.onicecandidate = ({ candidate }) =>
+    this.pc1.addIceCandidate(candidate);
+
+  const offer = await this.pc1.createOffer();
+  await this.pc2.setRemoteDescription(offer);
+  await this.pc1.setLocalDescription(offer);
+
+  const answer = await this.pc2.createAnswer();
+  await this.pc1.setRemoteDescription(answer);
+  await this.pc2.setLocalDescription(answer);
+};
+
+VideoPipe.prototype.close = function () {
+  this.pc1.close();
+  this.pc2.close();
+};
+
 
 const worker = new Worker("./js/worker.js", { name: "E2EE worker" });
 
@@ -70,32 +87,15 @@ function setupReceiverTransform(receiver) {
   ]);
 }
 
-function VideoPipe(stream, forceSend, forceReceive, handler) {
-  this.pc1 = new RTCPeerConnection({ encodedInsertableStreams: forceSend });
-  this.pc2 = new RTCPeerConnection({ encodedInsertableStreams: forceReceive });
-  this.pc2.ontrack = handler;
-  stream.getTracks().forEach((track) => this.pc1.addTrack(track, stream));
+let startToMiddle;
+let startToEnd;
+
+let localStream;
+
+if (!!!window.RTCRtpScriptTransform) {
+  const stream = new ReadableStream();
+  window.postMessage(stream, "*", [stream]);
 }
-
-VideoPipe.prototype.negotiate = async function () {
-  this.pc1.onicecandidate = ({ candidate }) =>
-    this.pc2.addIceCandidate(candidate);
-  this.pc2.onicecandidate = ({ candidate }) =>
-    this.pc1.addIceCandidate(candidate);
-
-  const offer = await this.pc1.createOffer();
-  await this.pc2.setRemoteDescription(offer);
-  await this.pc1.setLocalDescription(offer);
-
-  const answer = await this.pc2.createAnswer();
-  await this.pc1.setRemoteDescription(answer);
-  await this.pc2.setLocalDescription(answer);
-};
-
-VideoPipe.prototype.close = function () {
-  this.pc1.close();
-  this.pc2.close();
-};
 
 callButton.onclick = function call() {
   callButton.disabled = true;
@@ -114,8 +114,6 @@ callButton.onclick = function call() {
   startToEnd = new VideoPipe(localStream, true, true, (e) => {
     const { receiver, streams } = e;
     setupReceiverTransform(receiver);
-
-    remoteStream = streams[0];
     video2.srcObject = streams[0];
   });
 
