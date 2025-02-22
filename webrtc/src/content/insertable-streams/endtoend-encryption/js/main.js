980d06c11407562996e12063184db508e38ac2a8
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-22 17:47:53 +0900

    xxx

--- webrtc/src/content/insertable-streams/endtoend-encryption/js/main.js
+++ webrtc/src/content/insertable-streams/endtoend-encryption/js/main.js
@@ -100,9 +100,9 @@ function setupReceiverTransform(receiver) {
   ]);
 }
 
-function maybeSetCodecPreferences(trackEvent) {
+function maybeSetCodecPreferences({ track, transceiver }) {
   if (!SupportsSetCodecPreferences) return;
-  if (trackEvent.track.kind === "audio" && preferredAudioCodecMimeType) {
+  if (track.kind === "audio" && preferredAudioCodecMimeType) {
     const { codecs } = RTCRtpReceiver.getCapabilities("audio");
     const selectedCodecIndex = codecs.findIndex(
       (c) => c.mimeType === preferredAudioCodecMimeType
@@ -110,8 +110,8 @@ function maybeSetCodecPreferences(trackEvent) {
     const selectedCodec = codecs[selectedCodecIndex];
     codecs.splice(selectedCodecIndex, 1);
     codecs.unshift(selectedCodec);
-    trackEvent.transceiver.setCodecPreferences(codecs);
-  } else if (trackEvent.track.kind === "video" && preferredVideoCodecMimeType) {
+    transceiver.setCodecPreferences(codecs);
+  } else if (track.kind === "video" && preferredVideoCodecMimeType) {
     const { codecs } = RTCRtpReceiver.getCapabilities("video");
     const selectedCodecIndex = codecs.findIndex(
       (c) => c.mimeType === preferredVideoCodecMimeType
@@ -119,43 +119,41 @@ function maybeSetCodecPreferences(trackEvent) {
     const selectedCodec = codecs[selectedCodecIndex];
     codecs.splice(selectedCodecIndex, 1);
     codecs.unshift(selectedCodec);
-    trackEvent.transceiver.setCodecPreferences(codecs);
+    transceiver.setCodecPreferences(codecs);
   }
 }
 
 function VideoPipe(stream, forceSend, forceReceive, handler) {
-  this.pc1 = new RTCPeerConnection({ encodedInsertableStreams: forceSend })
-  this.pc2 = new RTCPeerConnection({ encodedInsertableStreams: forceReceive })
-  this.pc2.ontrack = handler
-  stream.getTracks().forEach((track) => this.pc1.addTrack(track, stream))
+  this.pc1 = new RTCPeerConnection({ encodedInsertableStreams: forceSend });
+  this.pc2 = new RTCPeerConnection({ encodedInsertableStreams: forceReceive });
+  this.pc2.ontrack = handler;
+  stream.getTracks().forEach((track) => this.pc1.addTrack(track, stream));
 }
 
 VideoPipe.prototype.negotiate = async function () {
-  this.pc1.onicecandidate = (e) => this.pc2.addIceCandidate(e.candidate)
-  this.pc2.onicecandidate = (e) => this.pc1.addIceCandidate(e.candidate)
+  this.pc1.onicecandidate = (e) => this.pc2.addIceCandidate(e.candidate);
+  this.pc2.onicecandidate = (e) => this.pc1.addIceCandidate(e.candidate);
 
-  const offer = await this.pc1.createOffer()
-  await this.pc2.setRemoteDescription(offer)
-  await this.pc1.setLocalDescription(offer)
+  const offer = await this.pc1.createOffer();
+  await this.pc2.setRemoteDescription(offer);
+  await this.pc1.setLocalDescription(offer);
 
-  const answer = await this.pc2.createAnswer()
-  await this.pc1.setRemoteDescription(answer)
-  await this.pc2.setLocalDescription(answer)
-}
+  const answer = await this.pc2.createAnswer();
+  await this.pc1.setRemoteDescription(answer);
+  await this.pc2.setLocalDescription(answer);
+};
 
 VideoPipe.prototype.close = function () {
-  this.pc1.close()
-  this.pc2.close()
-}
+  this.pc1.close();
+  this.pc2.close();
+};
 
 callButton.onclick = function call() {
   callButton.disabled = true;
   hangupButton.disabled = false;
   console.log("Starting call");
-  // The real use case is where the middle box relays the
-  // packets and listens in, but since we don't have
-  // access to raw packets, we just send the same video
-  // to both places.
+  // 実際の使用例は、中央のボックスがパケットを中継してリッスンすることですが、生のパケットにアクセスできないため、同じビデオを両方の場所に送信するだけです。
+  // The real use case is where the middle box relays the packets and listens in, but since we don't have access to raw packets, we just send the same video to both places.
   startToMiddle = new VideoPipe(localStream, true, false, (e) => {
     // Do not setup the receiver transform.
     maybeSetCodecPreferences(e);
