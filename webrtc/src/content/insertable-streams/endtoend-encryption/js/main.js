d7ef1adb7b5031710162f9162976d57a15c43e64
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-22 16:53:45 +0900

    xxxx

--- webrtc/src/content/insertable-streams/endtoend-encryption/js/main.js
+++ webrtc/src/content/insertable-streams/endtoend-encryption/js/main.js
@@ -1,110 +1,117 @@
-'use strict'
+"use strict";
 
 /* global RTCRtpScriptTransform */
 /* global VideoPipe */
 
-const video1 = document.querySelector('video#video1')
-const video2 = document.querySelector('video#video2')
-const videoMonitor = document.querySelector('#video-monitor')
+const video1 = document.querySelector("video#video1");
+const video2 = document.querySelector("video#video2");
+const videoMonitor = document.querySelector("#video-monitor");
 
-const startButton = document.getElementById('startButton')
-const callButton = document.getElementById('callButton')
-const hangupButton = document.getElementById('hangupButton')
+const startButton = document.getElementById("startButton");
+const callButton = document.getElementById("callButton");
+const hangupButton = document.getElementById("hangupButton");
 
-const cryptoKey = document.querySelector('#crypto-key')
-const cryptoOffsetBox = document.querySelector('#crypto-offset')
-const banner = document.querySelector('#banner')
-const muteMiddleBox = document.querySelector('#mute-middlebox')
+const cryptoKey = document.querySelector("#crypto-key");
+const cryptoOffsetBox = document.querySelector("#crypto-offset");
+const banner = document.querySelector("#banner");
+const muteMiddleBox = document.querySelector("#mute-middlebox");
 
-startButton.onclick = start
-callButton.onclick = call
-hangupButton.onclick = hangup
+startButton.onclick = start;
+callButton.onclick = call;
+hangupButton.onclick = hangup;
 
-cryptoKey.addEventListener('change', setCryptoKey)
-cryptoOffsetBox.addEventListener('change', setCryptoKey)
-muteMiddleBox.addEventListener('change', toggleMute)
+cryptoKey.addEventListener("change", setCryptoKey);
+cryptoOffsetBox.addEventListener("change", setCryptoKey);
+muteMiddleBox.addEventListener("change", toggleMute);
 
-let startToMiddle
-let startToEnd
+let startToMiddle;
+let startToEnd;
 
-let localStream
+let localStream;
 // eslint-disable-next-line no-unused-vars
-let remoteStream
+let remoteStream;
 
 // Preferring a certain codec is an expert option without GUI.
 // Use opus by default.
 // eslint-disable-next-line prefer-const
-let preferredAudioCodecMimeType = 'audio/opus'
+let preferredAudioCodecMimeType = "audio/opus";
 // Use VP8 by default to limit depacketization issues.
 // eslint-disable-next-line prefer-const
-let preferredVideoCodecMimeType = 'video/VP8'
+let preferredVideoCodecMimeType = "video/VP8";
 
-const supportsSetCodecPreferences = window.RTCRtpTransceiver && 'setCodecPreferences' in window.RTCRtpTransceiver.prototype
+const supportsSetCodecPreferences =
+  window.RTCRtpTransceiver &&
+  "setCodecPreferences" in window.RTCRtpTransceiver.prototype;
 
-let hasEnoughAPIs = !!window.RTCRtpScriptTransform
+let hasEnoughAPIs = !!window.RTCRtpScriptTransform;
 
 if (!hasEnoughAPIs) {
-  const supportsInsertableStreams = !!RTCRtpSender.prototype.createEncodedStreams
+  const supportsInsertableStreams =
+    !!RTCRtpSender.prototype.createEncodedStreams;
 
-  let supportsTransferableStreams = false
+  let supportsTransferableStreams = false;
   try {
-    const stream = new ReadableStream()
-    window.postMessage(stream, '*', [stream])
-    supportsTransferableStreams = true
+    const stream = new ReadableStream();
+    window.postMessage(stream, "*", [stream]);
+    supportsTransferableStreams = true;
   } catch (e) {
-    console.error('Transferable streams are not supported.')
+    console.error("Transferable streams are not supported.");
   }
-  hasEnoughAPIs = supportsInsertableStreams && supportsTransferableStreams
+  hasEnoughAPIs = supportsInsertableStreams && supportsTransferableStreams;
 }
 
 if (!hasEnoughAPIs) {
-  banner.innerText = 'Your browser does not support WebRTC Encoded Transforms. ' + 'This sample will not work.'
-  if (adapter.browserDetails.browser === 'chrome') {
-    banner.innerText += ' Try with Enable experimental Web Platform features enabled from chrome://flags.'
+  banner.innerText =
+    "Your browser does not support WebRTC Encoded Transforms. " +
+    "This sample will not work.";
+  if (adapter.browserDetails.browser === "chrome") {
+    banner.innerText +=
+      " Try with Enable experimental Web Platform features enabled from chrome://flags.";
   }
-  startButton.disabled = true
-  cryptoKey.disabled = true
-  cryptoOffsetBox.disabled = true
+  startButton.disabled = true;
+  cryptoKey.disabled = true;
+  cryptoOffsetBox.disabled = true;
 }
 
 function gotStream(stream) {
-  console.log('Received local stream')
-  video1.srcObject = stream
-  localStream = stream
-  callButton.disabled = false
+  console.log("Received local stream");
+  video1.srcObject = stream;
+  localStream = stream;
+  callButton.disabled = false;
 }
 
 function gotRemoteStream(stream) {
-  console.log('Received remote stream')
-  remoteStream = stream
-  video2.srcObject = stream
+  console.log("Received remote stream");
+  remoteStream = stream;
+  video2.srcObject = stream;
 }
 
 function start() {
-  console.log('Requesting local stream')
-  startButton.disabled = true
-  const options = { audio: true, video: true }
+  console.log("Requesting local stream");
+  startButton.disabled = true;
+  const options = { audio: true, video: true };
   navigator.mediaDevices
     .getUserMedia(options)
     .then(gotStream)
     .catch(function (e) {
-      alert('getUserMedia() failed')
-      console.log('getUserMedia() error: ', e)
-    })
+      alert("getUserMedia() failed");
+      console.log("getUserMedia() error: ", e);
+    });
 }
 
 // We use a Worker to do the encryption and decryption.
 // See
 //   https://developer.mozilla.org/en-US/docs/Web/API/Worker
 // for basic concepts.
-const worker = new Worker('./js/worker.js', { name: 'E2EE worker' })
+const worker = new Worker("./js/worker.js", { name: "E2EE worker" });
 function setupSenderTransform(sender) {
   if (window.RTCRtpScriptTransform) {
-    sender.transform = new RTCRtpScriptTransform(worker, { operation: 'encode' })
-    return
+    sender.transform = new RTCRtpScriptTransform(worker, {
+      operation: "encode",
+    });
+    return;
   }
 
-  const senderStreams = sender.createEncodedStreams()
   // Instead of creating the transform stream here, we do a postMessage to the worker. The first
   // argument is an object defined by us, the second is a list of variables that will be transferred to
   // the worker. See
@@ -118,106 +125,103 @@ function setupSenderTransform(sender) {
       .pipeThrough(transformStream)
       .pipeTo(senderStreams.writable);
   */
-  const { readable, writable } = senderStreams
-  worker.postMessage(
-    {
-      operation: 'encode',
-      readable,
-      writable,
-    },
-    [readable, writable],
-  )
+  const { readable, writable } = sender.createEncodedStreams();
+  worker.postMessage({ operation: "encode", readable, writable }, [
+    readable,
+    writable,
+  ]);
 }
 
 function setupReceiverTransform(receiver) {
   if (window.RTCRtpScriptTransform) {
-    receiver.transform = new RTCRtpScriptTransform(worker, { operation: 'decode' })
-    return
+    receiver.transform = new RTCRtpScriptTransform(worker, {
+      operation: "decode",
+    });
+    return;
   }
 
-  const receiverStreams = receiver.createEncodedStreams()
-  const { readable, writable } = receiverStreams
-  worker.postMessage(
-    {
-      operation: 'decode',
-      readable,
-      writable,
-    },
-    [readable, writable],
-  )
+  const { readable, writable } = receiver.createEncodedStreams();
+  worker.postMessage({ operation: "decode", readable, writable }, [
+    readable,
+    writable,
+  ]);
 }
 
 function maybeSetCodecPreferences(trackEvent) {
-  if (!supportsSetCodecPreferences) return
-  if (trackEvent.track.kind === 'audio' && preferredAudioCodecMimeType) {
-    const { codecs } = RTCRtpReceiver.getCapabilities('audio')
-    const selectedCodecIndex = codecs.findIndex((c) => c.mimeType === preferredAudioCodecMimeType)
-    const selectedCodec = codecs[selectedCodecIndex]
-    codecs.splice(selectedCodecIndex, 1)
-    codecs.unshift(selectedCodec)
-    trackEvent.transceiver.setCodecPreferences(codecs)
-  } else if (trackEvent.track.kind === 'video' && preferredVideoCodecMimeType) {
-    const { codecs } = RTCRtpReceiver.getCapabilities('video')
-    const selectedCodecIndex = codecs.findIndex((c) => c.mimeType === preferredVideoCodecMimeType)
-    const selectedCodec = codecs[selectedCodecIndex]
-    codecs.splice(selectedCodecIndex, 1)
-    codecs.unshift(selectedCodec)
-    trackEvent.transceiver.setCodecPreferences(codecs)
+  if (!supportsSetCodecPreferences) return;
+  if (trackEvent.track.kind === "audio" && preferredAudioCodecMimeType) {
+    const { codecs } = RTCRtpReceiver.getCapabilities("audio");
+    const selectedCodecIndex = codecs.findIndex(
+      (c) => c.mimeType === preferredAudioCodecMimeType
+    );
+    const selectedCodec = codecs[selectedCodecIndex];
+    codecs.splice(selectedCodecIndex, 1);
+    codecs.unshift(selectedCodec);
+    trackEvent.transceiver.setCodecPreferences(codecs);
+  } else if (trackEvent.track.kind === "video" && preferredVideoCodecMimeType) {
+    const { codecs } = RTCRtpReceiver.getCapabilities("video");
+    const selectedCodecIndex = codecs.findIndex(
+      (c) => c.mimeType === preferredVideoCodecMimeType
+    );
+    const selectedCodec = codecs[selectedCodecIndex];
+    codecs.splice(selectedCodecIndex, 1);
+    codecs.unshift(selectedCodec);
+    trackEvent.transceiver.setCodecPreferences(codecs);
   }
 }
 
 function call() {
-  callButton.disabled = true
-  hangupButton.disabled = false
-  console.log('Starting call')
+  callButton.disabled = true;
+  hangupButton.disabled = false;
+  console.log("Starting call");
   // The real use case is where the middle box relays the
   // packets and listens in, but since we don't have
   // access to raw packets, we just send the same video
   // to both places.
   startToMiddle = new VideoPipe(localStream, true, false, (e) => {
     // Do not setup the receiver transform.
-    maybeSetCodecPreferences(e)
-    videoMonitor.srcObject = e.streams[0]
-  })
-  startToMiddle.pc1.getSenders().forEach(setupSenderTransform)
-  startToMiddle.negotiate()
+    maybeSetCodecPreferences(e);
+    videoMonitor.srcObject = e.streams[0];
+  });
+  startToMiddle.pc1.getSenders().forEach(setupSenderTransform);
+  startToMiddle.negotiate();
 
   startToEnd = new VideoPipe(localStream, true, true, (e) => {
-    setupReceiverTransform(e.receiver)
-    maybeSetCodecPreferences(e)
-    gotRemoteStream(e.streams[0])
-  })
-  startToEnd.pc1.getSenders().forEach(setupSenderTransform)
-  startToEnd.negotiate()
+    setupReceiverTransform(e.receiver);
+    maybeSetCodecPreferences(e);
+    gotRemoteStream(e.streams[0]);
+  });
+  startToEnd.pc1.getSenders().forEach(setupSenderTransform);
+  startToEnd.negotiate();
 
-  console.log('Video pipes created')
+  console.log("Video pipes created");
 }
 
 function hangup() {
-  console.log('Ending call')
-  startToMiddle.close()
-  startToEnd.close()
-  hangupButton.disabled = true
-  callButton.disabled = false
+  console.log("Ending call");
+  startToMiddle.close();
+  startToEnd.close();
+  hangupButton.disabled = true;
+  callButton.disabled = false;
 }
 
 function setCryptoKey(event) {
-  console.log('Setting crypto key to ' + cryptoKey.value)
-  const currentCryptoKey = cryptoKey.value
-  const useCryptoOffset = !cryptoOffsetBox.checked
+  console.log("Setting crypto key to " + cryptoKey.value);
+  const currentCryptoKey = cryptoKey.value;
+  const useCryptoOffset = !cryptoOffsetBox.checked;
   if (currentCryptoKey) {
-    banner.innerText = 'Encryption is ON'
+    banner.innerText = "Encryption is ON";
   } else {
-    banner.innerText = 'Encryption is OFF'
+    banner.innerText = "Encryption is OFF";
   }
   worker.postMessage({
-    operation: 'setCryptoKey',
+    operation: "setCryptoKey",
     currentCryptoKey,
     useCryptoOffset,
-  })
+  });
 }
 
 function toggleMute(event) {
-  video2.muted = muteMiddleBox.checked
-  videoMonitor.muted = !muteMiddleBox.checked
+  video2.muted = muteMiddleBox.checked;
+  videoMonitor.muted = !muteMiddleBox.checked;
 }
--- webrtc/src/content/insertable-streams/endtoend-encryption/js/worker.js
+++ webrtc/src/content/insertable-streams/endtoend-encryption/js/worker.js
@@ -16,99 +16,99 @@ let currentKeyIdentifier = 0;
 //
 // It makes the (encrypted) video and audio much more fun to watch and listen to
 // as the decoder does not immediately throw a fatal error.
-const frameTypeToCryptoOffset = {
-  key: 10,
-  delta: 3,
-  undefined: 1,
-};
+// const frameTypeToCryptoOffset = {
+//   key: 10,
+//   delta: 3,
+//   undefined: 1,
+// };
 
-function dump(encodedFrame, direction, max = 16) {
-  const data = new Uint8Array(encodedFrame.data);
-  let bytes = "";
-  for (let j = 0; j < data.length && j < max; j++) {
-    bytes += (data[j] < 16 ? "0" : "") + data[j].toString(16) + " ";
-  }
-  const metadata = encodedFrame.getMetadata();
-  console.log(
-    performance.now().toFixed(2),
-    direction,
-    bytes.trim(),
-    "len=" + encodedFrame.data.byteLength,
-    "type=" + (encodedFrame.type || "audio"),
-    "ts=" + (metadata.rtpTimestamp || encodedFrame.timestamp),
-    "ssrc=" + metadata.synchronizationSource,
-    "pt=" + (metadata.payloadType || "(unknown)"),
-    "mimeType=" + (metadata.mimeType || "(unknown)")
-  );
-}
+// function dump(encodedFrame, direction, max = 16) {
+//   const data = new Uint8Array(encodedFrame.data);
+//   let bytes = "";
+//   for (let j = 0; j < data.length && j < max; j++) {
+//     bytes += (data[j] < 16 ? "0" : "") + data[j].toString(16) + " ";
+//   }
+//   const metadata = encodedFrame.getMetadata();
+//   console.log(
+//     performance.now().toFixed(2),
+//     direction,
+//     bytes.trim(),
+//     "len=" + encodedFrame.data.byteLength,
+//     "type=" + (encodedFrame.type || "audio"),
+//     "ts=" + (metadata.rtpTimestamp || encodedFrame.timestamp),
+//     "ssrc=" + metadata.synchronizationSource,
+//     "pt=" + (metadata.payloadType || "(unknown)"),
+//     "mimeType=" + (metadata.mimeType || "(unknown)")
+//   );
+// }
 
-let scount = 0;
-function encodeFunction(encodedFrame, controller) {
-  // if (scount++ < 30) {
-  //   // dump the first 30 packets.
-  //   dump(encodedFrame, 'send')
-  // }
-  // if (currentCryptoKey) {
-  //   const view = new DataView(encodedFrame.data)
-  //   // Any length that is needed can be used for the new buffer.
-  //   const newData = new ArrayBuffer(encodedFrame.data.byteLength + 5)
-  //   const newView = new DataView(newData)
+// let scount = 0;
+// function encodeFunction(encodedFrame, controller) {
+//   // if (scount++ < 30) {
+//   //   // dump the first 30 packets.
+//   //   dump(encodedFrame, 'send')
+//   // }
+//   // if (currentCryptoKey) {
+//   //   const view = new DataView(encodedFrame.data)
+//   //   // Any length that is needed can be used for the new buffer.
+//   //   const newData = new ArrayBuffer(encodedFrame.data.byteLength + 5)
+//   //   const newView = new DataView(newData)
 
-  //   const cryptoOffset = useCryptoOffset ? frameTypeToCryptoOffset[encodedFrame.type] : 0
-  //   for (let i = 0; i < cryptoOffset && i < encodedFrame.data.byteLength; ++i) {
-  //     newView.setInt8(i, view.getInt8(i))
-  //   }
-  //   // This is a bitwise xor of the key with the payload. This is not strong encryption, just a demo.
-  //   for (let i = cryptoOffset; i < encodedFrame.data.byteLength; ++i) {
-  //     const keyByte = currentCryptoKey.charCodeAt(i % currentCryptoKey.length)
-  //     newView.setInt8(i, view.getInt8(i) ^ keyByte)
-  //   }
-  //   // Append keyIdentifier.
-  //   newView.setUint8(encodedFrame.data.byteLength, currentKeyIdentifier % 0xff)
-  //   // Append checksum
-  //   newView.setUint32(encodedFrame.data.byteLength + 1, 0xdeadbeef)
+//   //   const cryptoOffset = useCryptoOffset ? frameTypeToCryptoOffset[encodedFrame.type] : 0
+//   //   for (let i = 0; i < cryptoOffset && i < encodedFrame.data.byteLength; ++i) {
+//   //     newView.setInt8(i, view.getInt8(i))
+//   //   }
+//   //   // This is a bitwise xor of the key with the payload. This is not strong encryption, just a demo.
+//   //   for (let i = cryptoOffset; i < encodedFrame.data.byteLength; ++i) {
+//   //     const keyByte = currentCryptoKey.charCodeAt(i % currentCryptoKey.length)
+//   //     newView.setInt8(i, view.getInt8(i) ^ keyByte)
+//   //   }
+//   //   // Append keyIdentifier.
+//   //   newView.setUint8(encodedFrame.data.byteLength, currentKeyIdentifier % 0xff)
+//   //   // Append checksum
+//   //   newView.setUint32(encodedFrame.data.byteLength + 1, 0xdeadbeef)
 
-  //   encodedFrame.data = newData
-  // }
-  controller.enqueue(encodedFrame);
-}
+//   //   encodedFrame.data = newData
+//   // }
+//   controller.enqueue(encodedFrame);
+// }
 
-let rcount = 0;
-function decodeFunction(encodedFrame, controller) {
-  // if (rcount++ < 30) {
-  //   // dump the first 30 packets
-  //   dump(encodedFrame, 'recv')
-  // }
-  // const view = new DataView(encodedFrame.data)
-  // const checksum = encodedFrame.data.byteLength > 4 ? view.getUint32(encodedFrame.data.byteLength - 4) : false
-  // if (currentCryptoKey) {
-  //   if (checksum !== 0xdeadbeef) {
-  //     console.log('Corrupted frame received, checksum ' + checksum.toString(16))
-  //     return // This can happen when the key is set and there is an unencrypted frame in-flight.
-  //   }
-  //   const keyIdentifier = view.getUint8(encodedFrame.data.byteLength - 5)
-  //   if (keyIdentifier !== currentKeyIdentifier) {
-  //     console.log(`Key identifier mismatch, got ${keyIdentifier} expected ${currentKeyIdentifier}.`)
-  //     return
-  //   }
+// let rcount = 0;
+// function decodeFunction(encodedFrame, controller) {
+//   // if (rcount++ < 30) {
+//   //   // dump the first 30 packets
+//   //   dump(encodedFrame, 'recv')
+//   // }
+//   // const view = new DataView(encodedFrame.data)
+//   // const checksum = encodedFrame.data.byteLength > 4 ? view.getUint32(encodedFrame.data.byteLength - 4) : false
+//   // if (currentCryptoKey) {
+//   //   if (checksum !== 0xdeadbeef) {
+//   //     console.log('Corrupted frame received, checksum ' + checksum.toString(16))
+//   //     return // This can happen when the key is set and there is an unencrypted frame in-flight.
+//   //   }
+//   //   const keyIdentifier = view.getUint8(encodedFrame.data.byteLength - 5)
+//   //   if (keyIdentifier !== currentKeyIdentifier) {
+//   //     console.log(`Key identifier mismatch, got ${keyIdentifier} expected ${currentKeyIdentifier}.`)
+//   //     return
+//   //   }
 
-  //   const newData = new ArrayBuffer(encodedFrame.data.byteLength - 5)
-  //   const newView = new DataView(newData)
-  //   const cryptoOffset = useCryptoOffset ? frameTypeToCryptoOffset[encodedFrame.type] : 0
+//   //   const newData = new ArrayBuffer(encodedFrame.data.byteLength - 5)
+//   //   const newView = new DataView(newData)
+//   //   const cryptoOffset = useCryptoOffset ? frameTypeToCryptoOffset[encodedFrame.type] : 0
 
-  //   for (let i = 0; i < cryptoOffset; ++i) {
-  //     newView.setInt8(i, view.getInt8(i))
-  //   }
-  //   for (let i = cryptoOffset; i < encodedFrame.data.byteLength - 5; ++i) {
-  //     const keyByte = currentCryptoKey.charCodeAt(i % currentCryptoKey.length)
-  //     newView.setInt8(i, view.getInt8(i) ^ keyByte)
-  //   }
-  //   encodedFrame.data = newData
-  // } else if (checksum === 0xdeadbeef) {
-  //   return // encrypted in-flight frame but we already forgot about the key.
-  // }
-  controller.enqueue(encodedFrame);
-}
+//   //   for (let i = 0; i < cryptoOffset; ++i) {
+//   //     newView.setInt8(i, view.getInt8(i))
+//   //   }
+//   //   for (let i = cryptoOffset; i < encodedFrame.data.byteLength - 5; ++i) {
+//   //     const keyByte = currentCryptoKey.charCodeAt(i % currentCryptoKey.length)
+//   //     newView.setInt8(i, view.getInt8(i) ^ keyByte)
+//   //   }
+//   //   encodedFrame.data = newData
+//   // } else if (checksum === 0xdeadbeef) {
+//   //   return // encrypted in-flight frame but we already forgot about the key.
+//   // }
+//   controller.enqueue(encodedFrame);
+// }
 
 function handleTransform(operation, readable, writable) {
   if (operation === "encode") {
@@ -130,26 +130,16 @@ function handleTransform(operation, readable, writable) {
 
 // Handler for messages, including transferable streams.
 onmessage = ({ data }) => {
-  if (data.operation === "encode" || data.operation === "decode") {
-    return handleTransform(data.operation, data.readable, data.writable);
+  const { operation, readable, writable } = data;
+  if (operation === "encode" || operation === "decode") {
+    return handleTransform(operation, readable, writable);
   }
-  // if (data.operation === 'setCryptoKey') {
-  //   if (data.currentCryptoKey !== currentCryptoKey) {
-  //     currentKeyIdentifier++
-  //   }
-  //   currentCryptoKey = data.currentCryptoKey
-  //   useCryptoOffset = data.useCryptoOffset
-  // }
 };
 
 // Handler for RTCRtpScriptTransforms.
 if (self.RTCTransformEvent) {
   self.onrtctransform = (event) => {
-    const transformer = event.transformer;
-    handleTransform(
-      transformer.options.operation,
-      transformer.readable,
-      transformer.writable
-    );
+    const { options, readable, writable } = event.transformer;
+    handleTransform(options.operation, readable, writable);
   };
 }
